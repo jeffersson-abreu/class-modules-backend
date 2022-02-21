115853da6dd01328ce0f58687a13b8f693b8b218
ctw6av <ctw6av@gmail.com>
2022-02-21 16:51:36 -0400

    Added cors headers

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const controllers = require('./src/controllers');
 const database = require('./src/database');
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors')
+
 const app = express();
+
+app.use((req, res, next) => {
+  // Allow only ower front-end make requests in database. 
+  // All methods supported are also listed listed
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", 'GET,PATCH,POST,DELETE');
+  app.use(cors());
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
